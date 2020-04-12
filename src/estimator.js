415f62e68192a16bdab0f863209dc7aad13f06b8
Abduljafar Kamarudeen <abduljafar.kamaru@ekedp.com>
2020-04-12 09:40:18 +0100

    challenge 1-3

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,77 @@
-const covid19ImpactEstimator = (data) => data;
+
+const covid19ImpactEstimator = (data) => {
+  const currentlyInfected = data.reportedCases * 10;
+  const svrCurrentlyInfected = data.reportedCases * 50;
+  const totalBed = data.totalHospitalBeds;
+  const avgIncome = data.region.avgDailyIncomeInUSD;
+  const avgIncPop = data.region.avgDailyIncomePopulation;
+
+  let numDays = data.timeToElapse;
+  let dollarsInFlight;
+  let svrDollarsInFlight;
+  if (data.periodType === 'weeks') {
+    numDays *= 7;
+  }
+  if (data.periodType === 'months') {
+    numDays *= 30;
+  }
+
+  const infectionsByRequestedTime = currentlyInfected * (2 ** Math.floor(numDays / 3));
+  const svrInfectionsByRequestedTime = svrCurrentlyInfected * (2 ** Math.floor(numDays / 3));
+
+  const severeCasesByRequestedTime = Math.floor(0.15 * infectionsByRequestedTime);
+
+  const svrSevereCasesByRequestedTime = Math.floor(
+    0.15 * svrInfectionsByRequestedTime
+  );
+
+  const hospitalBedsByRequestedTime = Math.round(0.35 * totalBed) - severeCasesByRequestedTime;
+
+  const svrBedsByRequestedTime = Math.round(0.35 * totalBed) - svrSevereCasesByRequestedTime;
+
+  const casesForICUByRequestedTime = Math.floor(0.05 * infectionsByRequestedTime);
+  const svrCasesForICUByRequestedTime = Math.floor(
+    0.05 * svrInfectionsByRequestedTime
+  );
+
+  const casesForVentilatorsByRequestedTime = Math.floor(
+    0.02 * infectionsByRequestedTime
+  );
+
+  const svrCasesForVentilatorsByRequestedTime = Math.floor(
+    0.02 * svrInfectionsByRequestedTime
+  );
+
+  dollarsInFlight = infectionsByRequestedTime * avgIncPop * avgIncome * numDays;
+  dollarsInFlight = parseFloat(dollarsInFlight.toFixed(2));
+
+  svrDollarsInFlight = svrInfectionsByRequestedTime * avgIncPop * avgIncome * numDays;
+  svrDollarsInFlight = parseFloat(svrDollarsInFlight.toFixed(2));
+
+  return {
+    data: { ...data }, // the input data you got
+
+    impact: {
+      currentlyInfected,
+      infectionsByRequestedTime,
+      severeCasesByRequestedTime,
+      hospitalBedsByRequestedTime,
+      casesForICUByRequestedTime,
+      casesForVentilatorsByRequestedTime,
+      dollarsInFlight
+    }, // your best case estimation
+    severeImpact: {
+      currentlyInfected: svrCurrentlyInfected,
+      infectionsByRequestedTime: svrInfectionsByRequestedTime,
+      severeCasesByRequestedTime: svrSevereCasesByRequestedTime,
+      hospitalBedsByRequestedTime: svrBedsByRequestedTime,
+      casesForICUByRequestedTime: svrCasesForICUByRequestedTime,
+      casesForVentilatorsByRequestedTime: svrCasesForVentilatorsByRequestedTime,
+      dollarsInFlight: svrDollarsInFlight
+    } // your severe case estimation
+
+
+  };
+};
 
 export default covid19ImpactEstimator;
