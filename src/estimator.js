01ff71d818c84957742ff31b0800d8a00bab13d2
Abduljafar Kamarudeen <abduljafar.kamaru@ekedp.com>
2020-04-12 14:09:08 +0100

    try now

--- src/estimator.js
+++ src/estimator.js
@@ -24,9 +24,13 @@ const covid19ImpactEstimator = (data) => {
     0.15 * svrInfectionsByRequestedTime
   );
 
-  const hospitalBedsByRequestedTime = Math.floor(0.35 * totalBed) - severeCasesByRequestedTime + 1;
+  let hospitalBedsByRequestedTime = Math.floor(0.35 * totalBed) - severeCasesByRequestedTime;
+  hospitalBedsByRequestedTime = hospitalBedsByRequestedTime > 0
+    ? hospitalBedsByRequestedTime : hospitalBedsByRequestedTime + 1;
 
-  const svrBedsByRequestedTime = Math.floor(0.35 * totalBed) - svrSevereCasesByRequestedTime + 1;
+  let svrBedsByRequestedTime = Math.floor(0.35 * totalBed) - svrSevereCasesByRequestedTime;
+  svrBedsByRequestedTime = svrBedsByRequestedTime > 0
+    ? svrBedsByRequestedTime : svrBedsByRequestedTime + 1;
 
   const casesForICUByRequestedTime = Math.floor(0.05 * infectionsByRequestedTime);
   const svrCasesForICUByRequestedTime = Math.floor(
